6818ddcf48e219123d17ffafd05059e878a7e05a
damasio34 <damasio34@gmail.com>
2015-09-09 00:02:51 -0300

    Merge

--- js/services/services-value.js
+++ js/services/services-value.js
@@ -1,10 +1,31 @@
 (function(angular){
 
     var services = angular.module('wopo.services');
-    services.value('PARSE_CREDENTIALS', {
+    services.service('wopo-config', function(WebStorageService) {
 
-	    APP_ID: 'xQzgkV2A0PH4p4wdAadPvmazedrD4xp8dl1IA5PM',
-	    REST_API_KEY:'TgJ2wRXJaWJBjNLaDnxubD28xWIluHObEIOD447I'
+    	// PARSE_CREDENTIALS
+
+		// 'xQzgkV2A0PH4p4wdAadPvmazedrD4xp8dl1IA5PM'
+		// 'TgJ2wRXJaWJBjNLaDnxubD28xWIluHObEIOD447I'
+
+	    var APP_ID: this._getAppId();
+	    var REST_API_KEY: this._getRestApiKey();
+
+    	var _getAppId = function() {
+
+	    };
+
+	    var setAppId = function() {
+
+	    };
+
+	    var _getRestApiKey = function() {
+
+	    };
+
+	    var setRestApiKey = function() {
+
+	    };
 
 	});
 
